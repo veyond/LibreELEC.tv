989fc841205059fe9a9359d73e959f69394dec16
Jonas Karlman <jonas@kwiboo.se>
2017-10-08 22:12:54 +0200

    Rockchip: add linux rockchip-4.4

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -62,6 +62,12 @@ case "$LINUX" in
     PKG_SOURCE_DIR="xbian-sources-kernel-${PKG_COMMIT}*"   
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
+  rockchip-4.4)
+    PKG_VERSION="e7942c88"
+    PKG_URL="https://github.com/rockchip-linux/kernel/archive/$PKG_VERSION.tar.gz"
+    PKG_SOURCE_DIR="kernel-$PKG_VERSION*"
+    PKG_PATCH_DIRS="rockchip-4.4"
+    ;;
   *)
     PKG_VERSION="4.13"
     PKG_SHA256="2db3d6066c3ad93eb25b973a3d2951e022a7e975ee2fa7cbe5bddf84d9a49a2c"
